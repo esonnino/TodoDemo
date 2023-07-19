4291fca0a19d6453adfabb431e9b5fed922bd253
John Doe <jo@doe.com>
2023-07-19 09:42:05 -0700

    Confetti Animation

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ document.getElementById('newTaskInput').addEventListener('keydown', function(e) 
         e.preventDefault();
         var taskText = document.getElementById('newTaskInput').value;
         if (taskText.trim() !== '') {
-            addTask(taskText);
+            addTask(taskText, false);
+            document.getElementById('newTaskInput').classList.add('animated', 'bounce');
         }
     }
 });
 
-var todos = {
+var todos = JSON.parse(localStorage.getItem('todos')) || {
     today: [],
     week: {
         monday: [],
@@ -18,45 +19,65 @@ var todos = {
         friday: [],
         saturday: [],
         sunday: []
-    }
+    },
+    month: []
 };
 
-function addTask(taskText) {
+function addTask(taskText, completed) {
     var selectedMenu = document.querySelector('.menu-item.selected').id;
     var today = new Date().toLocaleString('en-US', { weekday: 'long' }).toLowerCase();
-
+    var newTask = { text: taskText, completed: completed };
+    
     if (selectedMenu === "menu-today") {
-        todos.today.push(taskText);
+        todos.today.push(newTask);
     } else if (selectedMenu === "menu-this-week") {
-        todos.week[today].push(taskText);
+        todos.week[today].push(newTask);
+    } else if (selectedMenu === "menu-this-month") {
+        todos.month.push(newTask);
     }
-
-    createTaskElement(taskText);
+    
+    localStorage.setItem('todos', JSON.stringify(todos));
+    createTaskElement(newTask, true);
     document.getElementById('newTaskInput').value = '';
+    document.getElementById('newTaskInput').classList.remove('animated', 'bounce');
 }
 
-function createTaskElement(taskText) {
+function createTaskElement(task, animate) {
     var li = document.createElement('li');
     li.className = 'todoItem';
+    if (animate) {
+        li.classList.add('todoItem-add', 'animated', 'slideInRight');
+    }
 
     var checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
     checkbox.addEventListener('change', function() {
         if (this.checked) {
+            task.completed = true;
             li.classList.add('completed');
+            createConfetti();
         } else {
+            task.completed = false;
             li.classList.remove('completed');
         }
+        localStorage.setItem('todos', JSON.stringify(todos));
     });
 
     var text = document.createElement('span');
-    text.textContent = taskText;
+    text.textContent = task.text;
     text.contentEditable = "false";
 
     var deleteBtn = document.createElement('button');
     deleteBtn.textContent = "Delete";
+    deleteBtn.className = "delete-button";
     deleteBtn.addEventListener('click', function() {
-        li.remove();
+        li.classList.add('todoItem-remove', 'animated', 'slideOutRight');
+        setTimeout(function() {
+            li.remove();
+            deleteTask(task);
+            animateTasksUp();
+        }, 1000);
     });
 
     li.appendChild(checkbox);
@@ -66,6 +87,24 @@ function createTaskElement(taskText) {
     document.getElementById('todoList').appendChild(li);
 }
 
+function deleteTask(task) {
+    var selectedMenu = document.querySelector('.menu-item.selected').id;
+    var today = new Date().toLocaleString('en-US', { weekday: 'long' }).toLowerCase();
+    
+    if (selectedMenu === "menu-today") {
+        var index = todos.today.findIndex(function (t) { return t.text === task.text; });
+        todos.today.splice(index, 1);
+    } else if (selectedMenu === "menu-this-week") {
+        var index = todos.week[today].findIndex(function (t) { return t.text === task.text; });
+        todos.week[today].splice(index, 1);
+    } else if (selectedMenu === "menu-this-month") {
+        var index = todos.month.findIndex(function (t) { return t.text === task.text; });
+        todos.month.splice(index, 1);
+    }
+    
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
 function switchList(menuId) {
     var ul = document.getElementById('todoList');
     ul.innerHTML = "";
@@ -73,19 +112,43 @@ function switchList(menuId) {
     switch(menuId) {
         case "menu-today":
             for (let todo of todos.today) {
-                createTaskElement(todo);
+                createTaskElement(todo, false);
             }
             break;
         case "menu-this-week":
             for (let day in todos.week) {
                 for (let todo of todos.week[day]) {
-                    createTaskElement(todo);
+                    createTaskElement(todo, false);
                 }
+            }
+            break;
+        case "menu-this-month":
+            for (let todo of todos.month) {
+                createTaskElement(todo, false);
             }
             break;
         default:
             break;
     }
+}
+
+function animateTasksUp() {
+    var todoList = document.getElementById('todoList').children;
+    for (var i = 0; i < todoList.length; i++) {
+        todoList[i].classList.add('animated', 'slideInUp');
+    }
+}
+
+function createConfetti() {
+    confetti({
+        particleCount: 100,
+        startVelocity: 30,
+        spread: 360,
+        ticks: 60,
+        origin: {
+            y: 0.5
+        }
+    });
 }
 
 var menuItems = document.getElementsByClassName('menu-item');
